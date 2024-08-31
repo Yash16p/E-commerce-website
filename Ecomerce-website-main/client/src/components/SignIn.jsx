6d16cde206dc8e0dd7a3f6c682ce3882eb1cdfea
Yash Pandey <143586974+Yash16p@users.noreply.github.com>
2024-08-31 10:17:15 +0530

    Update build files

--- Ecomerce-website-main/client/src/components/SignIn.jsx
+++ Ecomerce-website-main/client/src/components/SignIn.jsx
@@ -96,7 +96,7 @@ const SignIn = () => {
   return (
     <Container>
       <div>
-        <Title>Welcome to Krist 👋</Title>
+        <Title>Welcome to ecommzy 👋</Title>
         <Span>Please login with your details here</Span>
       </div>
       <div style={{ display: "flex", gap: "20px", flexDirection: "column" }}>
